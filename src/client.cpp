07bfb0d8fb00fe9139b4bcac6ef2da9c28368325
Lazar Nenovski <riliano@live.com>
2017-06-05 22:08:40 +0300

    Client: Fixed crashing when there is no bakground, activated fps counter

---
+++ src/client.cpp
@@ -0,0 +1,160 @@
+#include<iostream>
+#include<SDL2/SDL.h>
+#include<SDL2/SDL_image.h>
+#include<SDL2/SDL_net.h>
+
+SDL_Event e;
+SDL_Renderer* renderer;
+
+const int DEFAULT_PORT = 1234;
+IPaddress ip;
+TCPsocket client;
+SDLNet_SocketSet chkClient = SDLNet_AllocSocketSet( 1 );
+void ConnectToServer( std::string address = "localhost", Uint16 port = DEFAULT_PORT )
+{
+    std::cout<<"Connecting to: "<<address<<" on port "<<port<<std::endl;
+    SDLNet_ResolveHost( &ip, address.data(), port );	
+    client = SDLNet_TCP_Open( &ip );
+    if( !client )
+        std::cout<<"Failed to connect"<<std::endl;
+    else
+        SDLNet_TCP_AddSocket( chkClient, client );
+}
+
+#include "simpleGeometry.h"
+#include "struct.cpp"
+#include "init.h"
+#include "input.h"
+
+std::vector<texture_t> textures;
+
+std::vector<human_t> humanTemplates;
+std::vector<human_t> humans;
+std::vector<obsticle_t> roadblockTemplates;
+std::vector<obsticle_t> roadblock;
+
+SDL_Rect backgroundPos;
+SDL_Rect defBackgroundPos;
+texture_t background;
+float scale = 1;
+
+int netIDTable[2000];
+int roadblockIDTable[2000];
+int humanIDTable[2000];
+int textureIDTable[2000];
+
+int playerID;
+void ClientMain()
+{
+    SDL_Init( 0 );
+    InitInput();
+    InitWindow();
+    if( ShouldITurnVSync() )
+		renderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
+	else
+		renderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED );
+
+    ConnectToServer();
+
+	texture_t numbers( "Textures/numbers.png" );
+
+	long long inputT = SDL_GetTicks();
+	long long infoT = SDL_GetTicks();
+	long long fpsT = SDL_GetTicks();
+	long long drawSpellT = SDL_GetTicks();
+	long long checkNetT = SDL_GetTicks();
+    long long sendNetT = SDL_GetTicks();
+
+    int frames;
+    std::vector<int> sframes;
+	sframes.push_back(0);
+
+    while( true )
+    {
+        while( SDL_PollEvent( &e ) )
+        {
+            if( e.type == SDL_QUIT )
+            {
+                SDL_Quit();
+                return;
+            }
+        }
+        if( SDL_GetTicks() - inputT >= 10 and humans.size() > 0 )
+		{
+			ScanKeyboard();
+			AnalyzeInput( humans[playerID] );
+			ResetKeyboard();
+
+			inputT = SDL_GetTicks();
+		}
+
+        if( SDL_GetTicks() - sendNetT >= 10 )
+		{
+			if( humans.size() > 0 )
+			{
+				if( humans[playerID].active )
+				{
+					Uint8 meta[2] = {1, 4};
+					SDLNet_TCP_Send( client, meta, 2 );
+					char info[4] = {humans[playerID].movDirection[0], humans[playerID].movDirection[1], humans[playerID].attDirection, (char)humans[playerID].eqpSpell};
+					SDLNet_TCP_Send( client, info, 4 );
+					humans[playerID].movDirection[0] = 0;
+					humans[playerID].movDirection[1] = 0;
+					humans[playerID].attDirection = 0;
+					humans[playerID].active = false;
+				}
+			}
+			sendNetT = SDL_GetTicks();
+		}
+
+        if( SDL_GetTicks() - fpsT >= 1000 and ShouldIDisplayFPS() )
+		{
+			//std::cout<<"FPS: "<<frames<<std::endl;
+			std::vector<int> bucket;
+			sframes.clear();
+			while( frames > 0 )
+			{
+				bucket.push_back( frames%10 );
+				frames/=10;
+			}
+			for( int i=0;bucket.size()!=0;i++ )
+			{
+				sframes.push_back(bucket.back());
+				bucket.pop_back();
+			}
+			frames = 0;
+			fpsT = SDL_GetTicks();
+		}
+
+    	SDL_RenderCopy( renderer, background.texture, NULL, &backgroundPos );
+		for( int i = 0;i<roadblock.size();i++ )
+		{
+			roadblock[i].pos.x = humans[playerID].pos.x - humans[playerID].x + roadblock[i].x;// + (roadblock[i].pos.w - roadblock[i].w)/2;
+			roadblock[i].pos.y = humans[playerID].pos.y - humans[playerID].y + roadblock[i].y;// + (roadblock[i].pos.h - roadblock[i].h)/2;
+			SDL_RenderCopy( renderer, textures[roadblock[i].textureID].texture, &roadblock[i].frame, &roadblock[i].pos );
+		}
+        for( int i = 0;i<humans.size();i++ )
+		{
+			if( i!=playerID )
+			{
+				humans[i].pos.x = humans[playerID].pos.x - humans[playerID].x + humans[i].x;// + (humans[i].pos.w - humans[i].w)/2;
+				humans[i].pos.y = humans[playerID].pos.y - humans[playerID].y + humans[i].y;// + (humans[i].pos.h - humans[i].h)/2;
+			}
+			SDL_RenderCopy( renderer, textures[humans[i].textureID].texture, &humans[i].frame, &humans[i].pos );
+		}
+		
+		if( ShouldIDisplayFPS() )
+		{
+			for( int i=0;i<sframes.size();i++ )
+			{
+				SDL_Rect nextNumber = {sframes[i]*10, 0, 10, 18};
+				SDL_Rect nextNumberPos = {i*13 + 2, 5, 12, 20};
+				SDL_RenderCopy( renderer, numbers.texture, &nextNumber, &nextNumberPos );
+			}
+			frames++;
+		}
+
+        SDL_RenderPresent( renderer );
+		SDL_RenderClear( renderer );
+    }
+}
